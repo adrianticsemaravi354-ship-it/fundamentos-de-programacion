2cd58d22390a07d522d6a40dd9cb7dc7d18146dc
adrianticsemaravi354-ship-it <adrianticsemaravi354@gmail.com>
2026-04-27 09:05:34 -0500

    Add files via upload

--- Calculadora.java
+++ Calculadora.java
@@ -1,7 +1,7 @@
 public class Calculadora {
     public static void main(String[] args) {
-        int a = 20;
-        int b = 5;
+        int a = 12;
+        int b = 4;
         System.out.println("Suma: " + (a + b));
         System.out.println("Resta: " + (a - b));
         System.out.println("Multiplicación: " + (a * b));
